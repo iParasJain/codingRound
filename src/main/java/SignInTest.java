b65365681bd0160e631d89a423181005253e6ab8
iParasJain <paras99ims@gmail.com>
2018-09-05 17:51:29 +0530

    Adding SignTest with switchToframe & Verification

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,51 +1,38 @@
-import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SignInTest {
 
-    WebDriver driver = new ChromeDriver();
+	SelInstance selInst;
 
-    @Test
-    public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
+	@BeforeTest
+	public void launchChromeBrowser() {
+		selInst = new Utilities();
+		selInst.launchChromeBrowser();
+		PageFactory.initElements(selInst.getWebDriverInstance(), this);
+	}
 
-        setDriverPath();
+	@AfterTest
+	public void quitAllBrowsersSession() {
+		if (selInst.getWebDriverInstance() != null)
+			selInst.getWebDriverInstance().quit();
+	}
 
-        driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+	@Test
+	public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
+		selInst.navigateURL("https://www.cleartrip.com/");
+		selInst.waitForClickableElement(By.linkText("Your trips"), 10).click();
+		selInst.waitForClickableElement(By.id("SignIn"), 10).click();
+		selInst.waitFor(1000);
+		selInst.switchToIframeByLocator(By.id("modal_window"));
+		selInst.waitForClickableElement(By.id("signInButton"), 10).click();
+		String errors1 = selInst.waitForNonClickableElement(By.id("errors1"), 10).getText();
+		Assert.assertTrue(errors1.contains("There were errors in your submission"));
 
-        driver.findElement(By.linkText("Your trips")).click();
-        driver.findElement(By.id("SignIn")).click();
-
-        driver.findElement(By.id("signInButton")).click();
-
-        String errors1 = driver.findElement(By.id("errors1")).getText();
-        Assert.assertTrue(errors1.contains("There were errors in your submission"));
-        driver.quit();
-    }
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
-
+	}
 
 }
